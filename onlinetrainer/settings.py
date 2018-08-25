de472bd8e8d17b3a92b147478bd18fa1e6f39a37
DavianRobinson <davianr@gmail.com>
2018-08-25 14:16:22 +0100

    Update settings.py

--- onlinetrainer/settings.py
+++ onlinetrainer/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(-hm4ldlaj*dsn9vyry89ku_x$i*1#n&2#5accr2dzprnkrn2e'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
